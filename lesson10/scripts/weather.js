3d196dfcba988a6e296175269f4aac4805e8309f
teddy2j <teddy2jem@gmail.com>
2024-03-13 10:40:51 -0500

    week 10 first. Chamber nav bar updated

---
+++ lesson10/scripts/weather.js
@@ -0,0 +1,23 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&appid=146a25c754648df720367b60e1f0ca25';
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+        }
+        else {
+            throw Error(await response.text());
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
