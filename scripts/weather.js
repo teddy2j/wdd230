c851f8219bab73b52dbe5c3e19711c4617b402b6
teddy2j <teddy2jem@gmail.com>
2024-03-14 19:41:52 -0500

    week10 1

---
+++ scripts/weather.js
@@ -0,0 +1,35 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=-2.19&lon=-79.90&units=imperial&appid=146a25c754648df720367b60e1f0ca25';
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        }
+        else {
+            throw Error(await response.text());
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
+
+
+}
+
+apiFetch();
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${parseInt(data.main.temp)}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
+}
