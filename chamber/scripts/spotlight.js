5a56cb70d425f7d66a49476ba2c1d59f81be9891
teddy2j <teddy2jem@gmail.com>
2024-03-27 10:04:08 -0500

    final project!

--- chamber/scripts/spotlight.js
+++ chamber/scripts/spotlight.js
@@ -1,75 +1,92 @@
-const card = document.querySelectorAll(".ad");
 
 async function getAds() {
-    const response = await fetch("https://teddy2j.github.io/wdd230/data/links.json");
+    const response = await fetch("https://teddy2j.github.io/wdd230/chamber/data/members.json");
     const data = await response.json();
     const members = data.members; //array of objects
-    displayAds(members);
+    const spotlightTree = document.querySelectorAll(".ad"); //im getting an array of 3 references for the 3 html elements of class "ad"
+
+    displayAds(members, spotlightTree);
 
 }
 
 getAds();
 
 
-function displayAds(members) {
-    const ads = get3RamdomMembers(members);
+function displayAds(members, spotlightTree) {
+    const ads = get3RamdomMembers(members); //im getting an array of 3 objects
+    const tree = []; //este tendra un array de innerhtml que tendra cada iteracion del spotlightTree
     ads.forEach(ad => {
 
-        const elementsGroup = document.querySelectorAll(".ad"); //it will return an array of 3 references for the 3 html elements of class "ad"
-        elementsGroup.forEach(elementGroup => {
 
-            //creating html elements
-            const name = document.createElement('h3');
-            const img = document.createElement("img");
-            const membership = document.createElement("h4");
+        //mision, convertir estos 3 elementos (h3 img y h4) en un solo string y luego ponerlo en un array of strings, para luego en el nivel superior, appendarlos en spotlightTree
+        const container = document.createElement('div');
 
-            //creating elements' content
-            name.innerText = ad.name;
-            const filepath = `../${ad.image}`;
-            img.setAttribute("src", filepath);
-            membership.innerText = ad.membership;
+        // Crear elementos
+        const name = document.createElement('h3');
+        const img = document.createElement("img");
+        const membership = document.createElement("h4");
 
+        // Establecer contenido para los elementos
+        name.innerText = ad.name;
+        img.setAttribute("src", ad.image);
+        membership.innerText = ad.membership;
 
+        // Agregar los elementos al contenedor
+        container.appendChild(name);
+        container.appendChild(img);
+        container.appendChild(membership);
 
-            //appending
-            for (let i = 1; i <= 3; i++) {
-                elementGroup[i].appendChild(name);
-                elementGroup[i].appendChild(img);
-                elementGroup[i].appendChild(membership)
-            }
+        // Obtener el HTML del contenedor como una cadena de texto
+        const htmlString = container.innerHTML;
 
-        });
+        tree.push(htmlString);
 
     });
 
-
-}
-
-//get an array of objects, outputs another array of objects. It takes 3 random objects from an array of 7 objects.
-function get3RamdomMembers(members) {
-
-    //this is what i'll return. An array of 3 objects.
-    const group = [];
-
-
-    const randomNumbers = return3RandomNumbers();
-
-    for (let i = 0; i <= randomNumbers.length; i++) {
-        group.push(members[randomNumbers[i]]);
+    for (let i = 0; i < 3; i++) {
+        spotlightTree[i].innerHTML = tree[i];
     }
 
-    return group;
+    //get an array of objects, outputs another array of objects. It takes 3 random objects from an array of 7 objects.
+    function get3RamdomMembers(members) {
 
-}
+        //this is what i'll return. An array of 3 objects.
+        const group = [];
 
-function return3RandomNumbers() {
-    const randomNumbers = [];
-    while (randomNumbers.length < 3) {
-        const randomNumber = Math.floor(Math.random() * 7);
-        if (!randomNumbers.includes(randomNumber)) {
-            randomNumbers.push(randomNumber);
+
+        const randomNumbers = return3RandomNumbers();
+
+        for (let i = 0; i < randomNumbers.length; i++) {
+            group.push(members[randomNumbers[i]]);
         }
+
+        //verification
+        console.log("array of 3 objects::");
+        console.log(group);
+
+
+        return group;
+
     }
-    return randomNumbers;
+
+    function return3RandomNumbers() {
+        const randomNumbers = [];
+        while (randomNumbers.length < 3) {
+            const randomNumber = Math.floor(Math.random() * 7);
+            if (!randomNumbers.includes(randomNumber)) {
+                randomNumbers.push(randomNumber);
+            }
+        }
+
+        //verification
+        console.log("random numbers:");
+        console.log(randomNumbers);
+
+        return randomNumbers;
+
+
+    }
 
 }
+
+
