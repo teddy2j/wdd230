f72588e7e1179493936a853d01cc3f68cd79263a
teddy2j <teddy2jem@gmail.com>
2024-04-12 05:14:49 -0500

    final project 2

---
+++ final-project-scoots/scripts/hamburguer.js
@@ -0,0 +1,8 @@
+document.getElementById("lastModified").innerHTML = `Last modification: ${document.lastModified}`
+
+/* hamburguer menu */
+const button = document.getElementById("menu");
+const navigationBar = document.querySelector("nav");
+
+button.addEventListener("click", () => { button.classList.toggle("open"); navigationBar.classList.toggle("open") });
+
